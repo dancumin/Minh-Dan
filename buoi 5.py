09a4790e74a32ae350ec851fd9abea1c5fc451b7
Minh Đan <minhdannguyen088@gmail.com>
2026-05-27 18:02:38 +0700

    Update buoi 5.py

--- buoi 5.py
+++ buoi 5.py
@@ -18,8 +18,8 @@ print(f"\nb) Count of chars only in S1 (not in S2): {len(only_in_s1)}")
 print(f"   Count of chars only in S2 (not in S1): {len(only_in_s2)}")
 
 #c
-exclusive_S1 = {ch for ch in S1 if ch not in counter2}  # in S1, not in S2
-exclusive_S2 = {ch for ch in S2 if ch not in counter1}  # in S2, not in S1
+exclusive_s1 = {ch for ch in S1 if ch not in counter2}  # in S1, not in S2
+exclusive_s2 = {ch for ch in S2 if ch not in counter1}  # in S2, not in S1
 
-print(f"\nc) Chars only in S1: {exclusive_S1 if exclusive_s1 else 'None'}")
+print(f"\nc) Chars only in S1: {exclusive_s1 if exclusive_s1 else 'None'}")
 print(f"   Chars only in S2: {exclusive_S2 if exclusive_s2 else 'None'}")
