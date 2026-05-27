990382032d928a03240685c4e05950662dfe077d
Minh Đan <minhdannguyen088@gmail.com>
2026-05-27 18:04:35 +0700

    Update buoi 5.py

--- buoi 5.py
+++ buoi 5.py
@@ -22,4 +22,4 @@ exclusive_s1 = {ch for ch in S1 if ch not in counter2}  # in S1, not in S2
 exclusive_s2 = {ch for ch in S2 if ch not in counter1}  # in S2, not in S1
 
 print(f"\nc) Chars only in S1: {exclusive_s1 if exclusive_s1 else 'None'}")
-print(f"   Chars only in S2: {exclusive_S2 if exclusive_s2 else 'None'}")
+print(f"   Chars only in S2: {exclusive_s2 if exclusive_s2 else 'None'}")
